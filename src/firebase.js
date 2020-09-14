eb840999ea57275623087876ecf21801dad306cf
Kevin GRONDIN <kevin.grondin@reuniwatt.com>
2020-09-14 11:31:51 +0400

    📦add 3 articles

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAztHj09OVAbE63JoDWpUaPsX46afI1N2w",
+  authDomain: "kg-amaz.firebaseapp.com",
+  databaseURL: "https://kg-amaz.firebaseio.com",
+  projectId: "kg-amaz",
+  storageBucket: "kg-amaz.appspot.com",
+  messagingSenderId: "785081309435",
+  appId: "1:785081309435:web:bb1ff639a6f7c2f16fb85e",
+};
