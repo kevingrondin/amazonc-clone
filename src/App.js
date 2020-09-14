2d64e7adc7ae919ab7ae2196f30ed641bb7fa565
Kevin GRONDIN <kevin.grondin@reuniwatt.com>
2020-09-14 16:46:24 +0400

    📦add checkout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <>
+      <HashRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </>
   );
 }
 
